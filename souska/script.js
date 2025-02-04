de24c7f93ae7686dbd67f98cd91ae4421a1f6412
Zarlice <Zarlice@proton.me>
2025-02-04 04:35:22 +0100

    Delete souska/script.js

--- souska/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("confessButton").addEventListener("click", function() {
-  const responseMessage = document.getElementById("responseMessage");
-  responseMessage.textContent = "I’m so happy you feel the same way! ❤️";
-  responseMessage.classList.remove("hidden");
-});
